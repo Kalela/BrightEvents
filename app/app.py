6c4abfd4396ad75fedf3e3e5d949ecd82e1c72ae
Kalela <philipkalela@gmail.com>
2017-11-30 01:09:58 +0300

    Front end and improved apis

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, jsonify, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, DateField
 from wtforms.validators import InputRequired, Email, Length
 from entities import Users
 from entities import Events
+
 
 user = Users()
 app = Flask(__name__)
@@ -22,64 +23,74 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(),Email('Invalid Email')])
     password = PasswordField('Password',validators=[InputRequired()])
 
+class EventForm(FlaskForm):
+    eventname = StringField('Eventname', validators=[InputRequired()])
+    eventdate = DateField('mm/dd/yy',format='%m/%d/%Y')
+    eventlocation = StringField('Location', validators=[InputRequired()])
+    
+    
+    
+    
+    
 class my_apis(object):
     @app.route('/',methods=['GET'])
     def index():
         return render_template('index.html', title='Event Hub')
 
-
+    
+    
     @app.route('/api/v1/auth/login',methods=['POST', 'GET'])
-    def login():
+    def login():#add name feature
         form = LoginForm()
-        if request.method == 'POST':
-            print("Form username: ", form.username.data)
-            print("User.username:", user.username)
-            print("Form password: ", form.password.data)
-            print("Usr.password: ", user.password)
-            if form.validate_on_submit():           
-                if form.username.data==user.username and form.password.data==user.password:                
-                    return render_template('index.html', title='Event Hub')
-                else:
-                
-                    return '<h1>Review your login form or Sign Up</h1>'
+        if request.method == 'POST':          
+            if form.username.data==user.username and form.password.data==user.password:                
+                return render_template('dashboard.html', title='Event Hub'''',name=user.username''')
+            else:
+                return '<h1>Review your login form or Sign Up</h1>'
                 
         if request.method == 'GET':  
         
             return render_template('login.html',title='Login',form=form) 
     
-    @app.route('/api/v1/auth/register', methods=['GET','POST'])
+    @app.route('/api/v1/auth/register', methods=['POST', 'GET'])
     def register_page():
         form = RegistrationForm()
+        print (form.errors)
         if request.method == 'POST':
-            if form.validate_on_submit():
+            user.saveuser(form.username.data, form.password.data)
+            return render_template('login.html', title='Login', form=form)
             
-                user.saveuser(form.username.data, form.password.data)
-        if request.method == 'GET':    
+                
+        if request.method == 'GET':   
             return render_template('register.html', form=form)
-
+        
+    @app.route('/api/v1/auth/dashboard/<username>', methods=['POST', 'GET'])
+    def dashboard():
+        return render_template('dashboard.html')
+    
 
     @app.route('/api/v1/auth/logout',methods=['POST', 'GET'])
     def logout():
-	
-        return render_template('home.html', title='Home Page')
+        return render_template('index.html', title='Home Page')
 
     @app.route('/api/v1/auth/reset-password',methods=['POST','GET'])
     def reset_password():
-        return render_template('password.html',title='Log In')
-    '''password query page?'''
+        user.password = ''
+        return render_template('delete_password.html',title='Log In')
+    
+    @app.route('/api/v1/new_event/',methods=['POST', 'GET'])
+    def new_event():
+        form = EventForm()
+        return render_template('new_event.html', title="New Event", form=form)
 
-    @app.route('/api/events',methods=['POST', 'GET'])
-    def events():
-        return render_template('event.html',title='Event Page')
 
     @app.route('/api/v1/events/<int:eventid>',methods=['PUT'])
-    def eventid_():
+    def eventid():
         return render_template('event.html')
 
     @app.route('/api/v1/events/<int:eventid>',methods=['DELETE'])
     def eventid_post():
         return render_template('event.html',title='Log In')
-    '''change up eventid appropriately'''
 
 
     @app.route('/api/v1/events', methods=['GET', 'POST'])
@@ -88,7 +99,11 @@ class my_apis(object):
 
     @app.route('/api/v1/auth/logout', methods=['GET', 'POST'])
     def home_page():
-        return render_template('home.html')
+        return render_template('index.html')
+    
+    @app.route('/api/v1/about/',methods=['GET'])
+    def about():
+        return render_template('aboutus.html')
 
 
 
