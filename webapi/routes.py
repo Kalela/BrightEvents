3e18d712d6052c243dc93e7ece9bdd511c606e80
Kalela <philipkalela@gmail.com>
2017-12-20 18:13:14 +0300

    venv

--- webapi/routes.py
+++ webapi/routes.py
@@ -1,21 +1,24 @@
 #contains RESTful apis
 from flask_api import FlaskAPI
-from flask import jsonify, request, session
+from flask import Flask, jsonify, request, session, Blueprint
 from flasgger import Swagger, swag_from
 from api_data import Users
 from api_data import Events
 from api_documentation import Documentation
 
+api = Blueprint('api', __name__)
 app = FlaskAPI(__name__)
+app.register_blueprint(api, url_prefix='/api/v2')
 swagger = Swagger(app)
 users = Users()
 events = Events()
 docs = Documentation()
+app.config['SECRET_KEY'] = "mysecret"
 
 class MyApis(object):
     """Hold all api routes"""
     #Works
-    @app.route('/api/v2/auth/register', methods=['POST'])
+    @api.route('/auth/register', methods=['POST'])
     @swag_from(docs.register_dict)
     def register_page_json():
         """Add new users to data"""
@@ -27,10 +30,9 @@ class MyApis(object):
             else:
                 users.users.append(user)
                 return jsonify({'users':users.users}), 201
-    #{"username":"user" , "password":"123"} for input
 
     #Works
-    @app.route('/api/v2/auth/login', methods=['POST'])
+    @api.route('/auth/login', methods=['POST'])
     @swag_from(docs.login_dict)
     def login_json():
         """Login registered users"""
@@ -41,10 +43,9 @@ class MyApis(object):
             return jsonify("Logged in"), 201
         else:
             return jsonify("Please sign up or review your login info"), 401
-    #{"username":"user" , "password":"123"} for input
 
     #Works
-    @app.route('/api/v2/auth/logout', methods=['POST'])
+    @api.route('/auth/logout', methods=['POST'])
     @swag_from(docs.logout_dict)
     def logout_json():
         """Log out users"""
@@ -54,11 +55,9 @@ class MyApis(object):
             return jsonify("User logged out"), 201
         else:
             return jsonify('User is not logged in'), 200
-        #{'username':'kalela'}
-
 
     #Works
-    @app.route('/api/v2/auth/reset-password', methods=['POST'])
+    @api.route('/auth/reset-password', methods=['POST'])
     @swag_from(docs.pass_reset_dict)
     def reset_password_json():
         """Reset users password"""
@@ -67,17 +66,16 @@ class MyApis(object):
             old_user[request.form['username']] = request.form['password']
             if old_user in users.users:
                 user = {}
-                user[session['usernsme']] = request.form['new_password']
+                user[session['username']] = request.form['new_password']
                 users.users.append(user)
                 return jsonify({"Password changed from": old_user},{"To":user}), 201
             else:
                 return jsonify("User does not exist"), 404     
         else:
             return jsonify("Please log in"), 401
-        #pop old user and password
         
     #Works
-    @app.route('/api/v2/events', methods=['POST', 'GET'])
+    @api.route('/events', methods=['POST', 'GET'])
     @swag_from(docs.event_dict)
     def events_json():
         """Add or view events"""
@@ -99,18 +97,18 @@ class MyApis(object):
             return jsonify({'events':events}), 200
 
     #Fails
-    @app.route('/api/v2/events/<eventid>', methods=['PUT', 'DELETE'])
+    @api.route('/events/<eventid>', methods=['PUT', 'DELETE'])
     def event_update_json(eventid):#check if event exists
         """Edit existing events"""
         if request.method == 'PUT':
             i = 0
             while i < len(events.events):
                 try:
-                    if events[i][str(eventid)]:
-                        old_eventname = events[i][str(eventid)][0]
-                        old_date = events[i][str(eventid)][2] 
-                        old_location = events[i][str(eventid)][1] 
-                        old_category = events[i][str(eventid)][3]
+                    if events.events[i][str(eventid)]:
+                        old_eventname = events.events[i][str(eventid)][0]
+                        old_date = events.events[i][str(eventid)][2] 
+                        old_location = events.events[i][str(eventid)][1] 
+                        old_category = events.events[i][str(eventid)][3]
                         old_event = {}
                         old_event = {str(eventid):[old_eventname, old_location, old_date, old_category]}
                         events.events.remove(old_event)
@@ -134,16 +132,15 @@ class MyApis(object):
             i = 0
             while i < len(events.events):
                 try:
-                    if events[i][str(eventid)]: 
-                        eventname = events[i][str(eventid)][0]
-                        date = events[i][str(eventid)][2]
-                        location = events[i][str(eventid)][1]
-                        category = events[i][str(eventid)][3]
+                    if events.events[i][str(eventid)]: 
+                        eventname = events.events[i][str(eventid)][0]
+                        date = events.events[i][str(eventid)][2]
+                        location = events.events[i][str(eventid)][1]
+                        category = events.events[i][str(eventid)][3]
                         updated_event = {}
                         target_event = {str(eventid):[eventname, location, date]}
-                        print(target_event)
                         i += 1
-                        if target_event in events:
+                        if target_event in events.events:
                             events.events.pop(target_event)
                             return jsonify({"Events":events.events}), 201
                         else:
@@ -153,7 +150,7 @@ class MyApis(object):
                     pass
             
     #Fails with more than 1 event
-    @app.route('/api/v2/events/<eventid>/rsvp', methods=['POST'])
+    @api.route('/events/<eventid>/rsvp', methods=['POST'])
     def rsvp_json(eventid):
         """Send RSVPs to existing events"""
         if 'username' in session:
