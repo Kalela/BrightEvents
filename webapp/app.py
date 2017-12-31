913bf47ec40646c347e61eb258727c76deee7d16
Philip <philipkalela@gmail.com>
2017-12-31 14:31:02 +0300

    remove blueprints

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, session, Blueprint
+from flask import Flask, render_template, request, flash, session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, DateField, SelectField
@@ -9,9 +9,6 @@ from instance.config import app_config
 
 user = Users()
 event_object = Events()
-api = Blueprint('api', __name__)
-
-
 
 class LoginForm(FlaskForm):
     '''Create variables for wtforms Login form input'''
@@ -40,14 +37,15 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    app.config['SECRET_KEY'] = 'my-secret'
     Bootstrap(app)
     #Tested and working
     @app.route('/', methods=['GET'])
     def index():
-        return render_template('index.html', title='Event Hub')
+        return render_template('index.html', title='Event Hub'), 200
 
     #Tested and working
-    @api.route('/auth/register', methods=['POST', 'GET'])
+    @app.route('/api/v1/auth/register', methods=['POST', 'GET'])
     def register_page():
         """Register a new user"""
         form = RegistrationForm()
@@ -57,12 +55,12 @@ def create_app(config_name):
             usr = {}
             usr[str(usrname)] = str(pwd)
             user.users.append(usr)
-            return render_template('login.html', title='Login', form=form)
+            return render_template('login.html', title='Login', form=form), 201
 
         if request.method == 'GET':
-            return render_template('register.html', form=form)
+            return render_template('register.html', form=form), 200
 
-    @api.route('/auth/login', methods=['POST', 'GET'])
+    @app.route('/api/v1/auth/login', methods=['POST', 'GET'])
     def login():
         """Log a user in"""
         form = LoginForm()
@@ -86,7 +84,7 @@ def create_app(config_name):
         if request.method == 'GET':
             return render_template('login.html', title='Login', form=form)
 
-    @api.route('/auth/logout', methods=['POST', 'GET'])
+    @app.route('/api/v1/auth/logout', methods=['POST', 'GET'])
     def logout():
         """Log a user out"""
         if 'username' in session:
@@ -96,7 +94,7 @@ def create_app(config_name):
             flash("User already logged out")
             return render_template('index.html', title='Home Page')
 
-    @api.route('/auth/dashboard', methods=['POST', 'GET'])
+    @app.route('/api/v1/auth/dashboard', methods=['POST', 'GET'])
     def dashboard():
         """View sites dashboard"""
         if 'username' in session:
@@ -109,14 +107,14 @@ def create_app(config_name):
             name = user.guest
             return render_template('dashboard.html', logout=logout, name=name)
 
-    @api.route('/auth/reset-password', methods=['POST', 'GET'])
+    @app.route('/api/v1/auth/reset-password', methods=['POST', 'GET'])
     def reset_password():
         """Reset users password"""
         user.password = ''
         return render_template('delete_password.html', title='Log In')
     #Work on Reset Password
 
-    @api.route('/new_event', methods=['POST', 'GET'])
+    @app.route('/api/v1/new_event', methods=['POST', 'GET'])
     def new_event():
         """Create new user events"""
         form = EventForm()
@@ -147,7 +145,7 @@ def create_app(config_name):
         if request.method == 'GET':
             return render_template('new_event.html', form=form)
 
-    @api.route('/events/view', methods=['GET', 'POST', 'PUT', 'DELETE'])
+    @app.route('/api/v1/events/view', methods=['GET', 'POST', 'PUT', 'DELETE'])
     def event_view():
         """View and edit user events"""
         events = event_object.events
@@ -155,17 +153,17 @@ def create_app(config_name):
             return render_template('event_view.html', events=events)
 
     #view popular events(trailer)
-    @api.route('/events', methods=['GET', 'POST'])
+    @app.route('/api/v1/events', methods=['GET', 'POST'])
     def event_page():
         """Load a page for popular events"""
         return render_template('event.html')
 
-    @api.route('/about', methods=['GET'])
+    @app.route('/api/v1/about', methods=['GET'])
     def about():
         """Load about us page"""
         return render_template('aboutus.html')
 
-    @api.route('/send/<eventid>/rsvp', methods=['POST'])
+    @app.route('/api/v1/send/<eventid>/rsvp', methods=['POST'])
     def send_RSVP(eventid):
         """Send RSVP for an event for logged in users"""
         if request.method == 'POST':
@@ -183,8 +181,7 @@ def create_app(config_name):
                 name = user.guest
                 flash("Please Sign In or Sign Up")
                 return render_template('dashboard.html', logout=logout, name=name)
-            
-    app.register_blueprint(api, url_prefix='/api/v1')
+
     return app
 
 
