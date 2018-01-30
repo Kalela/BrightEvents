c07f76ac7da7934a5889553067ff8b9215f9ed8c
Kalela <philipkalela@gmail.com>
2018-01-30 18:09:00 +0300

    [chore] Remove url_prefix redundancy in tests

--- tests/event_tests.py
+++ tests/event_tests.py
@@ -1,3 +1,4 @@
+#Import dependancies
 import unittest
 import json
 
@@ -9,20 +10,21 @@ class TestEventEndpoints(unittest.TestCase):
         """Set up the application with configurations for testing"""
         self.app = create_app(config_name="testing")
         self.tester = self.app.test_client(self)
+        self.prefix = "/api/v2"
         with self.app.app_context():
             db.create_all()
 
     def register_and_login(self):
-        self.tester.post('/api/v2/auth/register',
+        self.tester.post('%s/auth/register' % self.prefix,
                                    data=dict(username = "admin",
                                              password = "1234",
                                              email = "test@email.com"))
-        tkn = self.tester.post('/api/v2/auth/login', data=dict(username = "admin",
+        tkn = self.tester.post('%s/auth/login' % self.prefix, data=dict(username = "admin",
                                                                password = "1234"))
         self.token = json.loads(tkn.data.decode())['access-token']
 
     def create_new_event(self):
-        return self.tester.post('/api/v2/events',
+        return self.tester.post('%s/events' % self.prefix,
                                    data=dict(eventname="newevent", location="newlocation", 
                                              date="2018/05/21", category="Social"),
                                    headers={'x-access-token':self.token})
@@ -37,7 +39,7 @@ class TestEventEndpoints(unittest.TestCase):
     def test_create_new_event_bad_category(self):
             """Test the create new event endpoint"""
             self.register_and_login()
-            response = self.tester.post('/api/v2/events',
+            response = self.tester.post('%s/events' % self.prefix,
                                    data=dict(eventname="newevent", location="newlocation", 
                                              date="2018/06/21", category="mycategory"),
                                    headers={'x-access-token':self.token})
@@ -48,7 +50,7 @@ class TestEventEndpoints(unittest.TestCase):
             """Test the creating the same event with a different date endpoint"""
             self.register_and_login()
             self.create_new_event()
-            response = self.tester.post('/api/v2/events',
+            response = self.tester.post('%s/events' % self.prefix,
                                    data=dict(eventname="newevent", location="newlocation", 
                                              date="2018/06/21", category="Social"),
                                    headers={'x-access-token':self.token})
@@ -58,7 +60,7 @@ class TestEventEndpoints(unittest.TestCase):
     def test_new_event_with_bad_format_input(self):
         """Test date or other input formatted wrong"""
         self.register_and_login()
-        response = self.tester.post('/api/v2/events',
+        response = self.tester.post('%s/events' % self.prefix,
                                data=dict(eventname = "newevent", location = "newlocation", 
                                          date = "21052018", category = "Social"),
                                          headers={'x-access-token':self.token})
@@ -76,7 +78,7 @@ class TestEventEndpoints(unittest.TestCase):
     def test_new_event_nologin(self):
         """Test the create new event endpoint with user not logged in"""
         self.register_and_login()
-        self.tester.post('/api/v2/auth/logout', headers={'x-access-token':self.token})
+        self.tester.post('%s/auth/logout' % self.prefix, headers={'x-access-token':self.token})
         response = self.create_new_event()
         self.assertEqual(response.status_code, 401)
         self.assertIn("Please Log In", str(response.data))
@@ -85,7 +87,7 @@ class TestEventEndpoints(unittest.TestCase):
         """Test the update existing event endpoint"""
         self.register_and_login()
         self.create_new_event()
-        response = self.tester.put('/api/v2/events/newevent',
+        response = self.tester.put('%s/events/newevent' % self.prefix,
                               data=dict(event_name = "myevent", 
                                         location = "mylocation", 
                                         date = "2018/03/19", 
@@ -98,7 +100,7 @@ class TestEventEndpoints(unittest.TestCase):
         """Test date or other input formatted wrong"""
         self.register_and_login()
         self.create_new_event()
-        response = self.tester.put('/api/v2/events/newevent',
+        response = self.tester.put('%s/events/newevent' % self.prefix,
                               data=dict(event_name = "myevent", 
                                         location = "mylocation", 
                                         date = "19032018", 
@@ -110,7 +112,7 @@ class TestEventEndpoints(unittest.TestCase):
     def test_update_event_does_not_exist(self):
         """Test update existing event if event does not exist"""
         self.register_and_login()
-        response = self.tester.put('/api/v2/events/newevent',
+        response = self.tester.put('%s/events/newevent' % self.prefix,
                               data=dict(event_name = "myevent", 
                                         location = "mylocation", 
                                         date = "2018/03/19", 
@@ -123,21 +125,21 @@ class TestEventEndpoints(unittest.TestCase):
         """Test the delete event endpoint"""
         self.register_and_login()
         self.create_new_event()
-        response = self.tester.delete('api/v2/events/newevent', headers={'x-access-token':self.token})
+        response = self.tester.delete('%s/events/newevent' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 205)
         self.assertIn("Event(s)", str(response.data))
 
     def test_delete_event_does_not_exist(self):
         """Test the delete event does not exist"""
         self.register_and_login()
-        response = self.tester.delete('api/v2/events/newevent', headers={'x-access-token':self.token})
+        response = self.tester.delete('%s/events/newevent' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 404)
         self.assertIn("does not exist", str(response.data))
 
     def test_view_events(self):
         """Test the view all events endpoint"""
         self.register_and_login()
-        response = self.tester.get('/api/v2/events', headers={'x-access-token':self.token})
+        response = self.tester.get('%s/events' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 200)
         self.assertIn("Events", str(response.data))
 
@@ -145,7 +147,7 @@ class TestEventEndpoints(unittest.TestCase):
         """Test the view all events endpoint"""
         self.register_and_login()
         q = "the"
-        response = self.tester.get('/api/v2/events', headers={'x-access-token':self.token})
+        response = self.tester.get('%s/events' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 200)
         self.assertIn("Events", str(response.data))
 
@@ -153,7 +155,7 @@ class TestEventEndpoints(unittest.TestCase):
         """Test the send rsvp endpoint"""
         self.register_and_login()
         self.create_new_event()
-        response = self.tester.post('/api/v2/events/newevent/rsvp', headers={'x-access-token':self.token},
+        response = self.tester.post('%s/events/newevent/rsvp' % self.prefix, headers={'x-access-token':self.token},
                                     queries={'owner':'admin'})
         self.assertEqual(response.status_code, 201)
         self.assertIn("RSVP sent", str(response.data))
@@ -162,15 +164,15 @@ class TestEventEndpoints(unittest.TestCase):
         """Test rsvp sent twice"""
         self.register_and_login()
         self.create_new_event()
-        self.tester.post('/api/v2/events/newevent/rsvp', headers={'x-access-token':self.token})
-        response = self.tester.post('/api/v2/events/newevent/rsvp', headers={'x-access-token':self.token})
+        self.tester.post('%s/events/newevent/rsvp' % self.prefix, headers={'x-access-token':self.token})
+        response = self.tester.post('%s/events/newevent/rsvp' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 409)
         self.assertIn("already sent", str(response.data))
 
     def test_rsvp_event_does_not_exist(self):
         """Test rsvp to a non-existent event"""
         self.register_and_login()
-        response = self.tester.post('/api/v2/events/newevent/rsvp', headers={'x-access-token':self.token})
+        response = self.tester.post('%s/events/newevent/rsvp' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 404)
         self.assertIn("does not exist", str(response.data))
 
@@ -178,8 +180,8 @@ class TestEventEndpoints(unittest.TestCase):
         """Test rsvp if a user is not logged in"""
         self.register_and_login()
         self.create_new_event()
-        self.tester.post('/api/v2/auth/logout', headers={'x-access-token':self.token})
-        response = self.tester.post('/api/v2/events/newevent/rsvp', headers={'x-access-token':self.token})
+        self.tester.post('%s/auth/logout' % self.prefix, headers={'x-access-token':self.token})
+        response = self.tester.post('%s/events/newevent/rsvp' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 401)
         self.assertIn("log in", str(response.data))
 
--- tests/user_tests.py
+++ tests/user_tests.py
@@ -9,25 +9,26 @@ class TestUserEndpoints(unittest.TestCase):
         """Set up the application with configurations for testing"""
         self.app = create_app(config_name="testing")
         self.tester = self.app.test_client(self)
+        self.prefix = "/api/v2"
         with self.app.app_context():
             db.create_all()
             
     def register_and_login(self, choice):
         if choice == "both":
-            self.tester.post('/api/v2/auth/register',
+            self.tester.post('%s/auth/register' % self.prefix,
                                        data=dict(username = "admin",
                                                  password = "1234",
                                                  email = "test@email.com"))
-            tkn = self.tester.post('/api/v2/auth/login', data=dict(username = "admin",
+            tkn = self.tester.post('%s/auth/login' % self.prefix, data=dict(username = "admin",
                                                                    password = "1234"))
             self.token = json.loads(tkn.data.decode())['access-token']
         
         if choice == "login":
-            return self.tester.post('/api/v2/auth/login', data=dict(username = "admin",
+            return self.tester.post('%s/auth/login' % self.prefix, data=dict(username = "admin",
                                                                    password = "1234"))
 
         if choice == "register":
-            return self.tester.post('/api/v2/auth/register',
+            return self.tester.post('%s/auth/register' % self.prefix,
                                        data=dict(username = "admin",
                                                  password = "1234",
                                                  email = "test@email.com"))
@@ -40,14 +41,14 @@ class TestUserEndpoints(unittest.TestCase):
     
     def test_register_noinput(self):
         """Test a blank input on register endpoint"""
-        response = self.tester.post('/api/v2/auth/register',
+        response = self.tester.post('%s/auth/register' % self.prefix,
                                data=dict(username = "", password = "1234", email = "test@email.com"))
         self.assertEqual(response.status_code, 400)
         self.assertIn("Please insert", str(response.data))
         
     def test_register_bad_email_input(self):
         """Test if email input on register endpoint is not valid"""
-        response = self.tester.post('/api/v2/auth/register',
+        response = self.tester.post('%s/auth/register' % self.prefix,
                                data=dict(username = "", password = "1234", email = "testemail.com"))
         self.assertEqual(response.status_code, 400)
         self.assertIn("insert a valid email", str(response.data))
@@ -69,7 +70,7 @@ class TestUserEndpoints(unittest.TestCase):
     def test_login_noinput(self):
         """Test theres no input for login endpoint"""
         self.register_and_login("register")
-        response = tester.post('/api/v2/auth/login',
+        response = tester.post('%s/auth/login' % self.prefix,
                                data=dict(username="", password="1234"))
         self.assertEqual(response.status_code, 401)
         self.assertIn("Could not verify", str(response.data))
@@ -83,7 +84,7 @@ class TestUserEndpoints(unittest.TestCase):
     def test_login_noinput(self):
         """Test theres no input for login endpoint"""
         self.register_and_login("register")
-        response = self.tester.post('/api/v2/auth/login',
+        response = self.tester.post('%s/auth/login' % self.prefix,
                                data=dict(username="admin", password="abcd"))
         self.assertEqual(response.status_code, 401)
         self.assertIn("Could not verify", str(response.data))
@@ -91,22 +92,22 @@ class TestUserEndpoints(unittest.TestCase):
     def test_logout(self):
         """Test the logout user endpoint"""
         self.register_and_login("both")
-        response = self.tester.post('/api/v2/auth/logout', headers={'x-access-token':self.token})
+        response = self.tester.post('%s/auth/logout' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 202)
         self.assertIn("logged out", str(response.data))
         
     def test_logout_twice(self):
         """Test the user is already logged out"""
         self.register_and_login("both")
-        self.tester.post('/api/v2/auth/logout', headers={'x-access-token':self.token})
-        response = self.tester.post('/api/v2/auth/logout', headers={'x-access-token':self.token})
+        self.tester.post('%s/auth/logout' % self.prefix, headers={'x-access-token':self.token})
+        response = self.tester.post('%s/auth/logout' % self.prefix, headers={'x-access-token':self.token})
         self.assertEqual(response.status_code, 200)
         self.assertIn("already logged out", str(response.data))
 
     def test_reset_password(self):
         """Test the reset password endpoint"""
         self.register_and_login("both")
-        response = self.tester.post('/api/v2/auth/reset-password',
+        response = self.tester.post('%s/auth/reset-password' % self.prefix,
                                headers={'x-access-token':self.token},
                                data=dict(new_password="somethingnew", confirm_password="somethingnew"))
         self.assertEqual(response.status_code, 205)
@@ -115,7 +116,7 @@ class TestUserEndpoints(unittest.TestCase):
     def test_reset_password_wrongconfirm(self):
         """Test the reset password endpoint if confirm and new don't match"""
         self.register_and_login("both")
-        response = self.tester.post('/api/v2/auth/reset-password',
+        response = self.tester.post('%s/auth/reset-password' % self.prefix,
                                headers={'x-access-token':self.token},
                                data=dict(new_password="somethingnew", confirm_password="omethingnew"))
         self.assertEqual(response.status_code, 409)
@@ -124,7 +125,7 @@ class TestUserEndpoints(unittest.TestCase):
     def test_reset_password_old(self):
         """Test the reset password endpoint if user input is same as old password"""
         self.register_and_login("both")
-        response = self.tester.post('/api/v2/auth/reset-password',
+        response = self.tester.post('%s/auth/reset-password' % self.prefix,
                                headers={'x-access-token':self.token},
                                data=dict(new_password="1234", confirm_password="1234"))
         self.assertEqual(response.status_code, 409)
@@ -134,7 +135,7 @@ class TestUserEndpoints(unittest.TestCase):
         """Test the reset password if user not logged in"""
         response = self.register_and_login("both")
         self.tester.post('/api/v2/auth/logout', headers={'x-access-token':self.token})
-        response = self.tester.post('/api/v2/auth/reset-password',
+        response = self.tester.post('%s/auth/reset-password' % self.prefix,
                                headers={'x-access-token':self.token},
                                data=dict(new_password="somethingnew", confirm_password="somethingnew"))
         self.assertEqual(response.status_code, 401)
